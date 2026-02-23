4bc52cb572f18fc96cef5105b026dc77ea8a07fd
diego <diego@LAPTOP-MNPKGAGF>
2026-02-23 09:53:04 +0100

    schema igual al main

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -96,19 +96,19 @@ CREATE TABLE Actividades (
     costo_no_socio DECIMAL(10,2) NOT NULL,
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE NOT NULL,
-    FOREIGN KEY (id_instalacion) REFERENCES Instalaciones(id_instalacion)
+    id_periodo INTEGER NOT NULL,
+    FOREIGN KEY (id_instalacion) REFERENCES Instalaciones(id_instalacion),
+    FOREIGN KEY (id_periodo) REFERENCES PeriodosInscripcion(id_periodo)
 );
 
 -- Tabla de periodos de inscripción
 CREATE TABLE PeriodosInscripcion (
     id_periodo INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_actividad INTEGER NOT NULL,
     nombre TEXT NOT NULL,
     descripcion TEXT NOT NULL,
-    tipo TEXT NOT NULL CHECK(tipo IN ('socio', 'no_socio')),
-    fecha_inicio DATE NOT NULL,
-    fecha_fin DATE NOT NULL,
-    FOREIGN KEY (id_actividad) REFERENCES Actividades(id_actividad)
+    fecha_inicio_socio DATE NOT NULL,
+    fecha_fin_socio DATE NOT NULL,
+    fecha_fin_noSocio DATE NOT NULL
 );
 
 -- Tabla de sesiones de actividades
