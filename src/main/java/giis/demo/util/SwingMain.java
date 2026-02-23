21ce1aef0c634921cf1b85cdf05552fadae03773
diego <diego@LAPTOP-MNPKGAGF>
2026-02-23 13:45:33 +0100

    Cambio en el main para cuadrar

--- src/main/java/giis/demo/util/SwingMain.java
+++ src/main/java/giis/demo/util/SwingMain.java
@@ -7,7 +7,6 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import giis.demo.tkrun.*;
-import javax.swing.JPanel;
 
 /**
  * Punto de entrada principal que incluye botones para la ejecucion de las pantallas 
@@ -53,19 +52,17 @@ public class SwingMain {
 		frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 		
 		JButton btnEjecutarTkrun = new JButton("Ejecutar giis.demo.tkrun");
-		btnEjecutarTkrun.setBounds(51, 27, 149, 23);
 		btnEjecutarTkrun.addActionListener(new ActionListener() { //NOSONAR codigo autogenerado
 			public void actionPerformed(ActionEvent e) {
 				CarrerasController controller=new CarrerasController(new CarrerasModel(), new CarrerasView());
 				controller.initController();
 			}
 		});
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 		frame.getContentPane().add(btnEjecutarTkrun);
 		
 			
 		JButton btnInicializarBaseDeDatos = new JButton("Inicializar Base de Datos en Blanco");
-		btnInicializarBaseDeDatos.setBounds(28, 61, 197, 23);
 		btnInicializarBaseDeDatos.addActionListener(new ActionListener() { //NOSONAR codigo autogenerado
 			public void actionPerformed(ActionEvent e) {
 				Database db=new Database();
@@ -75,7 +72,6 @@ public class SwingMain {
 		frame.getContentPane().add(btnInicializarBaseDeDatos);
 			
 		JButton btnCargarDatosIniciales = new JButton("Cargar Datos Iniciales para Pruebas");
-		btnCargarDatosIniciales.setBounds(28, 95, 205, 23);
 		btnCargarDatosIniciales.addActionListener(new ActionListener() { //NOSONAR codigo autogenerado
 			public void actionPerformed(ActionEvent e) {
 				Database db=new Database();
@@ -84,6 +80,7 @@ public class SwingMain {
 			}
 		});
 		frame.getContentPane().add(btnCargarDatosIniciales);
+		
 		
 		JButton planificarActividad = new JButton("Ver disponibilidad instalación (Socio)");
 		planificarActividad.addActionListener(new ActionListener() { //NOSONAR codigo autogenerado
@@ -97,7 +94,8 @@ public class SwingMain {
 				controller.initController();
 			}
 		});
-		
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		frame.getContentPane().add(planificarActividad);
 	}
 
 	public JFrame getFrame() { return this.frame; }
