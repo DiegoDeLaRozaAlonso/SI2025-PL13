07bec8e355a56d5d4a459bc4c082fbe95bae435d
UO282000 <uo282000@uniovi.es>
2026-02-24 16:47:54 +0100

    listaPeriodo comienzo

---
+++ src/main/java/cd/admin/pablo/listaPeriodo/ListaPeriodo.java
@@ -0,0 +1,66 @@
+package cd.admin.pablo.listaPeriodo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+
+public class ListaPeriodo {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListaPeriodo window = new ListaPeriodo();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ListaPeriodo() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 807, 559);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JList listPeriodo = new JList();
+		listPeriodo.setBounds(10, 116, 771, 345);
+		frame.getContentPane().add(listPeriodo);
+		
+		JLabel lblNewLabel = new JLabel("Periodo");
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 22));
+		lblNewLabel.setBounds(60, 40, 88, 30);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(158, 40, 184, 30);
+		frame.getContentPane().add(comboBox);
+		
+		JButton btnNewButton = new JButton("Volver");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton.setBounds(292, 472, 140, 37);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
