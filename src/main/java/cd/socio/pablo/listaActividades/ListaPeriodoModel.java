17f78084f34d7a7505202b9ed79a8f7852439428
UO282000 <UO282000@GI-AN-B7-01.aulasuo.uniovi.es>
2026-02-27 12:22:45 +0100

    añadido getLista

--- src/main/java/cd/socio/pablo/listaActividades/ListaPeriodoModel.java
+++ src/main/java/cd/socio/pablo/listaActividades/ListaPeriodoModel.java
@@ -1,6 +1,12 @@
 package cd.socio.pablo.listaActividades;
 
+import java.util.Date;
+import java.util.List;
+
+import giis.demo.tkrun.CarreraDisplayDTO;
+import giis.demo.util.ApplicationException;
 import giis.demo.util.Database;
+import giis.demo.util.Util;
 
 public class ListaPeriodoModel {
 
@@ -13,6 +19,31 @@ public class ListaPeriodoModel {
 				+ "nombre, fecha_inicio, fecha_fin, aforo, costo_socio, costo_no_socio "
 				+ "from Actividades where fecha_inicio <= ? OR fecha_fin >= ?;";
 		
+	}
+	
+	/**
+	 * Obtiene la lista de carreras activas en forma objetos para una fecha de inscripcion dada
+	 */
+	public List<ActividadDTO> getListaCarreras(PeriodoDTO periodoInicio, PeriodoDTO periodoFin) {
+		validateNotNull(periodoInicio, "");
+		validateNotNull(periodoFin, "");
+		String sql = "SELECT id,descr," 
+				+ " case when ?<inicio then ''" // antes de inscripcion
+				+ "   when ?<=fin then '(Abierta)'" // fase 1
+				+ "   when ?<fecha then '(Abierta)'" // fase 2
+				+ "   when ?=fecha then '(Abierta)'" // fase 3
+				+ "   else '' " // despues de fin carrera
+				+ " end as abierta" 
+				+ " from carreras  where fecha>=? order by id";
+		String d = Util.dateToIsoString(periodoInicio);
+		String d2 = Util.dateToIsoString(periodoFin);
+		return db.executeQueryPojo(ActividadDTO.class, sql, d, d, d, d, d);
+	}
+	
+	/* De uso general para validacion de objetos */
+	private void validateNotNull(Object obj, String msg) {
+		if (obj == null)
+			throw new ApplicationException(msg);
 	}
 	
 	
