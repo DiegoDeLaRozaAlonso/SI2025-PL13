bc0a31eadb5b6beb397d22d690bb5e7001e84d25
diego <diego@LAPTOP-MNPKGAGF>
2026-04-20 18:57:44 +0200

    Añadido estado a Inscripciones

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -235,39 +235,39 @@ INSERT INTO Reservas (id_reserva, id_socio, id_instalacion, fecha_hora_inicio, d
 ------------------------------------------------------------
 -- INSCRIPCIONES (25) - TODO EN PUNTO
 ------------------------------------------------------------
-INSERT INTO Inscripciones (id_inscripcion, id_actividad, id_socio, nombre_no_socio, dni, fecha_inscripcion, pagado, tipo) VALUES
-(1, 1, 1, NULL, NULL, '2026-01-22 12:00', 1, 'socio'),
-(2, 1, 2, NULL, NULL, '2026-01-23 10:00', 1, 'socio'),
-(3, 1, 3, NULL, NULL, '2026-01-24 16:00', 0, 'socio'),
-(4, 1, 4, NULL, NULL, '2026-01-25 09:00', 1, 'socio'),
-(5, 1, 5, NULL, NULL, '2026-01-25 11:00', 0, 'socio'),
+INSERT INTO Inscripciones (id_inscripcion, id_actividad, id_socio, nombre_no_socio, dni, fecha_inscripcion, estado, pagado, tipo) VALUES
+(1, 1, 1, NULL, NULL, '2026-01-22 12:00', 'admitido', 1, 'socio'),
+(2, 1, 2, NULL, NULL, '2026-01-23 10:00', 'admitido', 1, 'socio'),
+(3, 1, 3, NULL, NULL, '2026-01-24 16:00', 'lista_espera', 0, 'socio'),
+(4, 1, 4, NULL, NULL, '2026-01-25 09:00', 'admitido', 1, 'socio'),
+(5, 1, 5, NULL, NULL, '2026-01-25 11:00', 'admitido', 0, 'socio'),
 
-(6, 2, 6, NULL, NULL, '2026-02-06 09:00', 0, 'socio'),
-(7, 2, 7, NULL, NULL, '2026-02-07 12:00', 0, 'socio'),
-(8, 2, 8, NULL, NULL, '2026-02-08 18:00', 1, 'socio'),
-(9, 2, 9, NULL, NULL, '2026-02-09 13:00', 1, 'socio'),
+(6, 2, 6, NULL, NULL, '2026-02-06 09:00', 'admitido', 0, 'socio'),
+(7, 2, 7, NULL, NULL, '2026-02-07 12:00', 'lista_espera', 0, 'socio'),
+(8, 2, 8, NULL, NULL, '2026-02-08 18:00', 'admitido', 1, 'socio'),
+(9, 2, 9, NULL, NULL, '2026-02-09 13:00', 'admitido', 1, 'socio'),
 
-(10, 3, 10, NULL, NULL, '2026-02-25 16:00', 0, 'socio'),
-(11, 3, 11, NULL, NULL, '2026-02-26 09:00', 1, 'socio'),
-(12, 3, 12, NULL, NULL, '2026-02-26 20:00', 0, 'socio'),
+(10, 3, 10, NULL, NULL, '2026-02-25 16:00', 'admitido', 0, 'socio'),
+(11, 3, 11, NULL, NULL, '2026-02-26 09:00', 'admitido', 1, 'socio'),
+(12, 3, 12, NULL, NULL, '2026-02-26 20:00', 'lista_espera', 0, 'socio'),
 
-(13, 4, 13, NULL, NULL, '2026-02-03 10:00', 1, 'socio'),
-(14, 4, 14, NULL, NULL, '2026-02-04 11:00', 1, 'socio'),
-(15, 4, 15, NULL, NULL, '2026-02-05 18:00', 0, 'socio'),
+(13, 4, 13, NULL, NULL, '2026-02-03 10:00', 'admitido', 1, 'socio'),
+(14, 4, 14, NULL, NULL, '2026-02-04 11:00', 'admitido', 1, 'socio'),
+(15, 4, 15, NULL, NULL, '2026-02-05 18:00', 'lista_espera', 0, 'socio'),
 
-(16, 5, 1, NULL, NULL, '2026-01-29 19:00', 1, 'socio'),
-(17, 5, 2, NULL, NULL, '2026-01-30 09:00', 0, 'socio'),
+(16, 5, 1, NULL, NULL, '2026-01-29 19:00', 'admitido', 1, 'socio'),
+(17, 5, 2, NULL, NULL, '2026-01-30 09:00', 'admitido', 0, 'socio'),
 
-(18, 6, 3, NULL, NULL, '2026-02-10 08:00', 0, 'socio'),
-(19, 6, 4, NULL, NULL, '2026-02-12 12:00', 1, 'socio'),
+(18, 6, 3, NULL, NULL, '2026-02-10 08:00', 'admitido', 0, 'socio'),
+(19, 6, 4, NULL, NULL, '2026-02-12 12:00', 'admitido', 1, 'socio'),
 
--- No socios (con DNI) - Estos ya los tenías bien
-(20, 1, NULL, 'Javier Santos', '66778899P', '2026-01-27 18:00', 1, 'no_socio'),
-(21, 1, NULL, 'Marta Díaz', '77889900Q', '2026-01-28 11:00', 1, 'no_socio'),
-(22, 2, NULL, 'Lucía Herrera', '88990011R', '2026-02-10 10:00', 1, 'no_socio'),
-(23, 4, NULL, 'Óscar Núñez', '99001122S', '2026-02-08 09:00', 0, 'no_socio'),
-(24, 5, NULL, 'Paula Rivas', '10111213T', '2026-02-02 20:00', 1, 'no_socio'),
-(25, 6, NULL, 'Diego Torres', '12131415U', '2026-02-18 17:00', 1, 'no_socio');
+-- No socios (con DNI)
+(20, 1, NULL, 'Javier Santos', '66778899P', '2026-01-27 18:00', 'admitido', 1, 'no_socio'),
+(21, 1, NULL, 'Marta Díaz', '77889900Q', '2026-01-28 11:00', 'admitido', 1, 'no_socio'),
+(22, 2, NULL, 'Lucía Herrera', '88990011R', '2026-02-10 10:00', 'admitido', 1, 'no_socio'),
+(23, 4, NULL, 'Óscar Núñez', '99001122S', '2026-02-08 09:00', 'lista_espera', 0, 'no_socio'),
+(24, 5, NULL, 'Paula Rivas', '10111213T', '2026-02-02 20:00', 'admitido', 1, 'no_socio'),
+(25, 6, NULL, 'Diego Torres', '12131415U', '2026-02-18 17:00', 'admitido', 1, 'no_socio');
 
 ------------------------------------------------------------
 -- RECIBOS (24)
